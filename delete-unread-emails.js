3d759566cbd1470baee335e67bcd461e92b81ad8
anshul1790 <45200300+anshul1790@users.noreply.github.com>
2023-07-03 11:15:31 +0530

    Add files via upload

---
+++ delete-unread-emails.js
@@ -0,0 +1,33 @@
+var count = 0;
+var totalEmailDeleteCount = 100;
+var deleteEmails = function() {
+    var unreadEmailList = document.querySelectorAll('.zA.zE');
+    unreadEmailList.forEach(function (emailItem) {
+        var isItemImportantTag = emailItem.querySelector('[data-is-important="true"]')
+        if (!isItemImportantTag) {
+            emailItem.querySelector('[data-tooltip="Delete"]').click();
+            count++;
+        }
+    });
+    console.log("Email deleted count is ", count);
+    if (count > totalEmailDeleteCount) {
+        console.log("Total email deletion limit breached");
+        clearInterval(interval);
+    }
+}
+var count = 0;
+var totalEmailDeleteCount = 100;
+var interval = setInterval(deleteEmails, 10000);
+
+// let clickEvent = new MouseEvent("click", {
+//     bubbles: true,
+//     cancelable: true,
+//     clientX: 200,
+//     clientY: 900
+//   });
+// document.querySelectorAll('.zA.zE')[0].querySelector('[data-tooltip="Delete"]').click()
+// deleteButton = document.querySelector('[data-tooltip="Delete"] .asa')
+// deleteButton.dispatchEvent(clickEvent)
+
+// k = document.querySelectorAll('.zA.zE')[0];
+// k.querySelector('[data-tooltip="Select"]').click()
